b1e6e5e839bcb26444e5f2762153707570e6666b
Abhijit Bhattacharyya <abhihere@gmail.com>
2016-05-11 17:49:56 +0530

    some modification. plot is generated yet main menu is lost......

--- src/moncgui/GeomController.java
+++ src/moncgui/GeomController.java
@@ -31,19 +31,19 @@ public class GeomController implements Initializable {
 
     private MoncGUI myGUI;
     private Scene geoScene;
-    private Stage geoStage ;
-    private CameraView camV = new CameraView();
+    private Stage geoStage;
+    private CameraView camV = new CameraView ();
 
-    private Stage newStage;
+    private Stage newStage = new Stage ();
     private Scene newScene;
     private Axis3D axis;
 
-    private final ScrollBar scB = new ScrollBar();
-    private final SplitPane paramPane1 = new SplitPane();
-    private final SplitPane drawPane = new SplitPane();
-    private final GridPane paramPane = new GridPane();
-    private final Group axisGroup = new Group();
-    private final Group rootGr = new Group();
+    private final ScrollBar scB = new ScrollBar ();
+    private final SplitPane paramPane1 = new SplitPane ();
+    private final SplitPane drawPane = new SplitPane ();
+    private final GridPane paramPane = new GridPane ();
+    private final Group axisGroup = new Group ();
+    private final Group rootGr = new Group ();
 
     double anchorX, anchorY, anchorAngle;
     //private final double cameraDistance = -1000.0;
@@ -63,29 +63,29 @@ public class GeomController implements Initializable {
     private double centerY;
     private double centerZ;
     private double scaleX = 0.08, scaleY = 0.08, scaleZ = 0.08;
-    private Rotate rotateX = new Rotate(0, Rotate.X_AXIS);
-    private Rotate rotateY = new Rotate(0, Rotate.Y_AXIS);
-    private Rotate rotateZ = new Rotate(0, Rotate.Z_AXIS);
+    private Rotate rotateX = new Rotate (0, Rotate.X_AXIS);
+    private Rotate rotateY = new Rotate (0, Rotate.Y_AXIS);
+    private Rotate rotateZ = new Rotate (0, Rotate.Z_AXIS);
     private int radSample = 10, lenSample = 3;
 
     private ArrayList<Sphere> sphList;
     private ArrayList<Cylinder> cylList;
     private ArrayList<Shape3D> myShapeList;
     private ArrayList<PhongMaterial> MaterialList
-            = new ArrayList<PhongMaterial>();
-    private List<String> materialNames = new ArrayList<String>();
+            = new ArrayList<PhongMaterial> ();
+    private List<String> materialNames = new ArrayList<String> ();
 
-    private Label BaseCoord = new Label("Origin");
-    private TextField baseCX = new TextField();
-    private TextField baseCY = new TextField();
-    private TextField baseCZ = new TextField();
-    private Label radT = new Label("Radius");
-    private TextField rad = new TextField();
-    private Label heightT = new Label("Height");
-    private TextField ht = new TextField();
-    private Label matT = new Label("Material");
-    private ComboBox<String> matList = new ComboBox<String>();
-    private Button drawMe = new Button("Draw");
+    private Label BaseCoord = new Label ("Origin");
+    private TextField baseCX = new TextField ();
+    private TextField baseCY = new TextField ();
+    private TextField baseCZ = new TextField ();
+    private Label radT = new Label ("Radius");
+    private TextField rad = new TextField ();
+    private Label heightT = new Label ("Height");
+    private TextField ht = new TextField ();
+    private Label matT = new Label ("Material");
+    private ComboBox<String> matList = new ComboBox<String> ();
+    private Button drawMe = new Button ("Draw");
 
     @FXML
     private Button shapeCLS;
@@ -106,14 +106,13 @@ public class GeomController implements Initializable {
         this.myGUI = myGUI;
     }
 
-    public void setMyScene(Scene scene) {
-        //this.geoScene = scene;
-        this.geoScene = scene;
-    }
-
+    /*
+     * public void setMyScene(Scene scene) { //this.geoScene = scene;
+     * this.geoScene = scene; }
+     */
     public void setMyStage(Stage stage) {
         this.geoStage = stage;
-        System.out.println("setMyStage :  Height = " + geoStage.getHeight()+"  Width = "+geoStage.getWidth());
+        System.out.println (geoStage.getScene ().getWindow ().widthProperty ());
     }
 
     /**
@@ -121,40 +120,21 @@ public class GeomController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-        buildScene();
-        /*
-        geoStage.setTitle ("Testing");
-        final Scene geoScene = new Scene (camV, drawWidth, drawHeight, true);
-        geoScene.setFill (new RadialGradient (225, 0.85, centerX, centerY,
-                drawWidth, false,
-                CycleMethod.NO_CYCLE, new Stop[]{new Stop (0f, Color.BLUE),
-                    new Stop (1f, Color.LIGHTBLUE)}));
-        buildCamera (geoScene);
-        Context3D context = Context3D.getInstance (camV);
-        lightSetting(context);
-        axis = buildAxes ();
-        camV.add (axis);
-         */
+        buildScene ();
+        Scene  geoScene = null;
     }
 
-    private void fixSetup() {
-        geoStage.setTitle("Testing");
-        System.out.println("fixSetup :  Height = " + geoStage.getHeight()+"  Width = "+geoStage.getWidth());
-        System.out.println("Scene Height " + geoStage.getScene().getHeight()+" Width "+geoStage.getScene().getWidth());
-        newScene = geoStage.getScene();
-        
-        System.out.println("Scene Height " + geoScene.getHeight()+" Width "+geoScene.getWidth());
-        final Scene geoScene = new Scene(camV, drawWidth, drawHeight, true);
-        geoScene.setFill(new RadialGradient(225, 0.85, centerX, centerY,
-                drawWidth, false,
-                CycleMethod.NO_CYCLE, new Stop[]{new Stop(0f, Color.BLUE),
-                    new Stop(1f, Color.LIGHTBLUE)}));
-        buildCamera(geoScene);
-        Context3D context = Context3D.getInstance(camV);
-        lightSetting(context);
-        axis = buildAxes();
-        camV.add(axis);
+    private Scene setMyScene(double wid, double ht) {
+        Scene sc = new Scene (camV, wid, ht, true);
+        sc.setFill (new RadialGradient (225, 0.85, centerX, centerY,
+                wid, false,
+                CycleMethod.NO_CYCLE, new Stop[]{new Stop (0f, Color.BLUE),
+                    new Stop (1f, Color.LIGHTBLUE)}));
+        System.out.println("passed setMyScene....");
+        System.out.println (
+                "SetMyScene:: Scene Height " + sc.getHeight () + " Width " +
+                sc.getWidth ());
+        return sc;
     }
 
     private void buildScene() {
@@ -170,95 +150,91 @@ public class GeomController implements Initializable {
         scaleX = 0.08;
         scaleY = 0.08;
         scaleZ = 0.08;
-        paramPane1.setPrefSize(menuWidth, menuHeight);
-        paramPane1.setOrientation(Orientation.VERTICAL);
-        paramPane.setGridLinesVisible(false);
-        paramPane.setPrefSize(menuWidth, menuHeight);
-        paramPane.setHgap(1);
-        paramPane.setVgap(3);
-        paramPane.setPadding(new Insets(0, 1, 0, 1));
-        paramPane1.getItems().add(paramPane);
+        paramPane1.setPrefSize (menuWidth, menuHeight);
+        paramPane1.setOrientation (Orientation.VERTICAL);
+        paramPane.setGridLinesVisible (false);
+        paramPane.setPrefSize (menuWidth, menuHeight);
+        paramPane.setHgap (1);
+        paramPane.setVgap (3);
+        paramPane.setPadding (new Insets (0, 1, 0, 1));
+        paramPane1.getItems ().add (paramPane);
 
-        drawPane.setPrefSize(drawWidth, drawHeight);
+        drawPane.setPrefSize (drawWidth, drawHeight);
 
-        geoMainArea.setLeft(paramPane);
-        geoMainArea.setRight(drawPane);
+        geoMainArea.setLeft (paramPane);
+        geoMainArea.setRight (drawPane);
     }
 
     private void buildCamera(Scene scene) {
-        scene.setCamera(new PerspectiveCamera());
+        scene.setCamera (new PerspectiveCamera ());
     }
 
     private void lightSetting(Context3D context) {
-        context.lighting = new Lighting3D();
-        context.lighting.add(Lighting3D.Type.DIFFUSE,
-                Lighting3D.Source.PARALLEL, 1.5, new Vector3D(1, 0.8, 0.6));
-        context.lighting.add(Lighting3D.Type.DIFFUSE,
-                Lighting3D.Source.PARALLEL, 1.0, new Vector3D(-1, -0.8, 0.6));
-        context.lighting.add(Lighting3D.Type.DIFFUSE,
-                Lighting3D.Source.PARALLEL, 0.5, new Vector3D(0, -0.2, -0.8));
-        context.showLights();
-        context.setShowBorders(true);
-        context.setShowTexts(true);
+        context.lighting = new Lighting3D ();
+        context.lighting.add (Lighting3D.Type.DIFFUSE,
+                Lighting3D.Source.PARALLEL, 1.5, new Vector3D (1, 0.8, 0.6));
+        context.lighting.add (Lighting3D.Type.DIFFUSE,
+                Lighting3D.Source.PARALLEL, 1.0, new Vector3D (-1, -0.8, 0.6));
+        context.lighting.add (Lighting3D.Type.DIFFUSE,
+                Lighting3D.Source.PARALLEL, 0.5, new Vector3D (0, -0.2, -0.8));
+        context.showLights ();
+        context.setShowBorders (true);
+        context.setShowTexts (true);
     }
 
     private Axis3D buildAxes() {
-        axis = new Axis3D(50.0, Color.AQUAMARINE);
+        axis = new Axis3D (50.0, Color.AQUAMARINE);
         return axis;
     }
 
     public void drawCylSolid() {
 
-        BaseCoord.setFont(new Font("Times New Roman", 11));
-        radT.setFont(new Font("Times New Roman", 11));
-        heightT.setFont(new Font("Times New Roman", 11));
-        matT.setFont(new Font("Times New Roman", 11));
+        BaseCoord.setFont (new Font ("Times New Roman", 11));
+        radT.setFont (new Font ("Times New Roman", 11));
+        heightT.setFont (new Font ("Times New Roman", 11));
+        matT.setFont (new Font ("Times New Roman", 11));
 
-        baseCX.setPrefColumnCount(6);
-        baseCX.setAlignment(Pos.CENTER_RIGHT);
-        baseCY.setPrefColumnCount(6);
-        baseCY.setAlignment(Pos.CENTER_RIGHT);
-        baseCZ.setPrefColumnCount(6);
-        baseCZ.setAlignment(Pos.CENTER_RIGHT);
-        rad.setPrefColumnCount(9);
-        rad.setAlignment(Pos.CENTER_RIGHT);
-        rad.setMaxSize(60, 1); // width height
-        ht.setPrefColumnCount(9);
-        ht.setAlignment(Pos.CENTER_RIGHT);
-        ht.setMaxSize(60, 1); // width height
+        baseCX.setPrefColumnCount (6);
+        baseCX.setAlignment (Pos.CENTER_RIGHT);
+        baseCY.setPrefColumnCount (6);
+        baseCY.setAlignment (Pos.CENTER_RIGHT);
+        baseCZ.setPrefColumnCount (6);
+        baseCZ.setAlignment (Pos.CENTER_RIGHT);
+        rad.setPrefColumnCount (9);
+        rad.setAlignment (Pos.CENTER_RIGHT);
+        rad.setMaxSize (60, 1); // width height
+        ht.setPrefColumnCount (9);
+        ht.setAlignment (Pos.CENTER_RIGHT);
+        ht.setMaxSize (60, 1); // width height
 
-        HBox hb1 = new HBox(baseCX, baseCY, baseCZ);
-        hb1.setSpacing(1); //hb1.setPadding(new Insets(2));
-        VBox vb1 = new VBox(BaseCoord, hb1);
+        HBox hb1 = new HBox (baseCX, baseCY, baseCZ);
+        hb1.setSpacing (1); //hb1.setPadding(new Insets(2));
+        VBox vb1 = new VBox (BaseCoord, hb1);
 
-        baseCX.setPromptText("0.0");
-        baseCY.setPromptText("0.0");
-        baseCZ.setPromptText("0.0");
-        rad.setPromptText("50.0");
-        ht.setPromptText("100.0");
+        baseCX.setPromptText ("0.0");
+        baseCY.setPromptText ("0.0");
+        baseCZ.setPromptText ("0.0");
+        rad.setPromptText ("50.0");
+        ht.setPromptText ("100.0");
         /*
-        paramPane.add (vb1, 0, 0); // col row
-        paramPane.add (radT, 0, 1);
-        paramPane.add (rad, 0, 2);
-        paramPane.add (heightT, 0, 3);
-        paramPane.add (ht, 0, 4);
-        paramPane.add (matT, 0, 5);
-        paramPane.add (matList, 0, 6);
-        paramPane.add (drawMe, 0, 7);
+         * paramPane.add (vb1, 0, 0); // col row paramPane.add (radT, 0, 1);
+         * paramPane.add (rad, 0, 2); paramPane.add (heightT, 0, 3);
+         * paramPane.add (ht, 0, 4); paramPane.add (matT, 0, 5); paramPane.add
+         * (matList, 0, 6); paramPane.add (drawMe, 0, 7);
          */
         rad0 = 90.0;
         len0 = 130.0;
-        radSample = 5;//2 * (int) rad0;
-        lenSample = 3;//(int) (len0 / 5.0);
+        radSample = 10;//2 * (int) rad0;
+        lenSample = 10;//(int) (len0 / 5.0);
 
         cylTest cyl
-                = new cylTest("cyl", lenSample, radSample, rad0, len0);
+                = new cylTest ("cyl", lenSample, radSample, rad0, len0);
         //cyl.getTransforms ().add (new Translate (centerX, -centerY, 10.0));
 
         // paramPane.getChildren ().removeAll (vb1, radT, rad, heightT, ht, matT, matList, drawMe);
-        camV.add(cyl);
+        camV.add (cyl);
 
-        drawMe.setOnAction(new EventHandler<ActionEvent>() {
+        drawMe.setOnAction (new EventHandler<ActionEvent> () {
             @Override
             public void handle(ActionEvent ev) {
                 /*
@@ -270,42 +246,53 @@ public class GeomController implements Initializable {
                  */
             }
         });
-        complete();
+        //complete ();
     }
 
-    void complete() {
-        camV.frameCam(geoStage, geoScene);
-        MouseHandler mouseHandler = new MouseHandler(geoScene, camV);
-        KeyHandler keyHandler = new KeyHandler(geoStage, geoScene, camV);
-        geoStage.setScene(geoScene);
-        geoStage.show();
+    void complete(Stage geoStage, Scene geoScene) {
+        camV.frameCam (geoStage, geoScene);
+        MouseHandler mouseHandler = new MouseHandler (geoScene, camV);
+        KeyHandler keyHandler = new KeyHandler (geoStage, geoScene, camV);
+        geoStage.setScene (geoScene);
+       // geoStage.show ();
     }
 
     @FXML
     void doShapeCyl(ActionEvent event) {
-        if (initialized == 0) {
-            System.out.println("Processing initialized=0");
-            fixSetup();
+        if ( initialized == 0 ) {
+            System.out.println ("In FXML :: Processing initialized=0");            
+            geoStage.setTitle ("Testing");
+
+            geoScene = setMyScene (drawWidth, drawHeight);
+            System.out.println ("in fxml :: Scene Height = " + geoScene.
+                    getHeight () + "  Width = " + geoScene.getWidth ());
+            buildCamera (geoScene);
+            Context3D context = Context3D.getInstance (camV);
+            lightSetting (context);
+            axis = buildAxes ();
+            camV.add (axis);
+
             initialized = 1;
         }
-        drawCylSolid();
+        drawCylSolid ();
+        complete (geoStage, geoScene);
     }
 
     public void drawCircSolid() {
-        if (initialized == 0) {
-            fixSetup();
+        if ( initialized == 0 ) {
+
             initialized = 1;
         }
     }
 
     @FXML
     private void doShapeCirc(ActionEvent event) {
-        drawCircSolid();
+        drawCircSolid ();
     }
 
     @FXML
     private void doDrawReset(MouseEvent event) {
-        drawPane.getItems().clear();
+        drawPane.getItems ().clear ();
 
     }
 }
