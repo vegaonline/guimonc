13f6049b80152be5fbfc14a816a113a5f8a9d8f6
Abhijit Bhattacharyya <abhihere@gmail.com>
2016-06-17 20:53:35 +0530

    Adding elements and compound database, modifying Material Controller...

--- src/moncgui/MatGeneratorController.java
+++ src/moncgui/MatGeneratorController.java
@@ -29,7 +29,7 @@ public class MatGeneratorController implements Initializable {
     private MoncGUI myGUI;
     private Stage matStage;
     private Scene matScene;
-    FileChooser fileChooser = new FileChooser();
+    FileChooser fileChooser = new FileChooser ();
     public FileWriter fileWriter;
     String fName = null;
     int selPressed = 0;
@@ -42,21 +42,23 @@ public class MatGeneratorController implements Initializable {
     private MenuItem seeMat;
     private MenuItem selectIt;
 
-    private Label matNameL = new Label("Material Name");
-    private TextField matName = new TextField();
-    private ListView<String> materials = new ListView();
-    private ListView<String> matSelected = new ListView();
-    private Label selMat = new Label("Select Element");
-    private Button selMe = new Button("Select");
-    private Button delMe = new Button("Delete");
-    private Button clsMe = new Button("Clear");
-    private Button acceptMe = new Button("Accept");
+    private Label matNameL = new Label ("Material Name");
+    private TextField matName = new TextField ();
+    private ListView<String> materials = new ListView ();
+    private ListView<String> matSelected = new ListView ();
+    private Label selMat = new Label ("Select Element");
+    private Button selMe = new Button ("Select");
+    private Button delMe = new Button ("Delete");
+    private Button clsMe = new Button ("Clear");
+    private Button acceptMe = new Button ("Accept");
 
-    private List<double[]> eleData1 = new ArrayList<double[]>();
-    private List<double[]> eleData2 = new ArrayList<double[]>();
-    private List<String> elename = new ArrayList<String>();
-    ObservableList<String> elementName = FXCollections.observableArrayList();
-    ObservableList<String> eleSelectName = FXCollections.observableArrayList();
+    private Map<String, Integer> datType = new HashMap<String, Integer>();    
+    ArrayList<Double> oneRow = new ArrayList<Double> ();
+    ObservableList<Double> eleData1 = FXCollections.observableArrayList (oneRow);
+    ObservableList<Double> eleData2 = FXCollections.observableArrayList (oneRow);
+    private List<String> elename = new ArrayList<String> ();
+    ObservableList<String> elementName = FXCollections.observableArrayList ();
+    ObservableList<String> eleSelectName = FXCollections.observableArrayList ();
 
     @FXML
     private AnchorPane matGenPane;
@@ -74,41 +76,41 @@ public class MatGeneratorController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        menuBar = new MenuBar();
-        matProcess = new Menu("Process material");
-        matSelect = new Menu("Select material for Simulation");
-        newMat = new MenuItem("Create Material");
-        delMat = new MenuItem("Delete Material");
-        seeMat = new MenuItem("Review Material");
-        selectIt = new MenuItem("Select Material");
+        menuBar = new MenuBar ();
+        matProcess = new Menu ("Process material");
+        matSelect = new Menu ("Select material for Simulation");
+        newMat = new MenuItem ("Create Material");
+        delMat = new MenuItem ("Delete Material");
+        seeMat = new MenuItem ("Review Material");
+        selectIt = new MenuItem ("Select Material");
 
-        matProcess.getItems().addAll(newMat, delMat, seeMat);
-        matSelect.getItems().add(selectIt);
-        menuBar.getMenus().addAll(matProcess, matSelect);
-        matGenPane.getChildren().add(menuBar);
+        matProcess.getItems ().addAll (newMat, delMat, seeMat);
+        matSelect.getItems ().add (selectIt);
+        menuBar.getMenus ().addAll (matProcess, matSelect);
+        matGenPane.getChildren ().add (menuBar);
 
-        newMat.setOnAction(new EventHandler<ActionEvent>() {
+        newMat.setOnAction (new EventHandler<ActionEvent> () {
             public void handle(ActionEvent ev) {
-                System.out.println(" I am in newmat");
-                makeNewMat();
+                System.out.println (" I am in newmat");
+                makeNewMat ();
             }
         });
 
-        delMat.setOnAction(new EventHandler<ActionEvent>() {
+        delMat.setOnAction (new EventHandler<ActionEvent> () {
             public void handle(ActionEvent ev) {
-                deleteMat();
+                deleteMat ();
             }
         });
 
-        seeMat.setOnAction(new EventHandler<ActionEvent>() {
+        seeMat.setOnAction (new EventHandler<ActionEvent> () {
             public void handle(ActionEvent ev) {
-                reviewMat();
+                reviewMat ();
             }
         });
 
-        selectIt.setOnAction(new EventHandler<ActionEvent>() {
+        selectIt.setOnAction (new EventHandler<ActionEvent> () {
             public void handle(ActionEvent ev) {
-                selMat();
+                selMat ();
             }
         });
     }
@@ -116,45 +118,45 @@ public class MatGeneratorController implements Initializable {
     public String matFileHandler(int act) {
         String fileName = null;
         File dataF;
-        File recordsDir = new File("Materials");
-        if (!recordsDir.exists()) {
-            recordsDir.mkdirs();
+        File recordsDir = new File ("Materials");
+        if ( !recordsDir.exists () ) {
+            recordsDir.mkdirs ();
         }
-        fileChooser.setInitialDirectory(recordsDir);
-        fileChooser.getExtensionFilters().clear();
-        FileChooser.ExtensionFilter extFilt = new FileChooser.ExtensionFilter(
+        fileChooser.setInitialDirectory (recordsDir);
+        fileChooser.getExtensionFilters ().clear ();
+        FileChooser.ExtensionFilter extFilt = new FileChooser.ExtensionFilter (
                 "Material Database (.mat)", "*.mat");
-        fileChooser.getExtensionFilters().add(extFilt);
+        fileChooser.getExtensionFilters ().add (extFilt);
 
         while (fileName == null && act == 1) { // open file
-            fileChooser.setTitle("Select Material database to Read data");
-            fileName = fileChooser.showOpenDialog(matStage).getPath();
+            fileChooser.setTitle ("Select Material database to Read data");
+            fileName = fileChooser.showOpenDialog (matStage).getPath ();
         }
         while (fileName == null && act == 2) { // save file
-            fileChooser.setTitle("Select Material database to save data");
-            fileName = fileChooser.showSaveDialog(matStage).getPath();
+            fileChooser.setTitle ("Select Material database to save data");
+            fileName = fileChooser.showSaveDialog (matStage).getPath ();
         }
-        if (fileName != null) {
+        if ( fileName != null ) {
             //dataF = new File(fileName);
             return fileName;
         } else {
-            popupMsg.infoBox("No file selected",
+            popupMsg.infoBox ("No file selected",
                     "File Saving Procedure not successful");
             return null;
         }
     }
 
     public void makeNewMat() {
-        popupMsg.infoBox(
+        popupMsg.infoBox (
                 "Material management is under development. Please have patience. Thank You",
                 "Feature is under development");
-        matGenPane.getChildren().clear();
-        matGenPane.getChildren().add(menuBar);
+        matGenPane.getChildren ().clear ();
+        matGenPane.getChildren ().add (menuBar);
 
     }
 
     public void deleteMat() {
-        popupMsg.infoBox(
+        popupMsg.infoBox (
                 "Material management is under development. Please have patience. Thank You",
                 "Feature is under development");
     }
@@ -162,110 +164,170 @@ public class MatGeneratorController implements Initializable {
     public void reviewMat() {
         File dataF = null;
         String fileName = null;
-        fileName = matFileHandler(1);
-        System.out.println(dataF);
-        popupMsg.infoBox(
+        fileName = matFileHandler (1);
+        System.out.println (dataF);
+        popupMsg.infoBox (
                 "Material management is under development. Please have patience. Thank You",
                 "Feature is under development");
     }
 
-    public void selMat() {
-        String fileName = null;
-        int dLen = 0;
-        File dataF = null;
+    public void getdataContents(File fl1, File fl2) {
         String line = null;
         String cvsSplitBy = ",";
-        fileName = matFileHandler(1); // open file
-        dataF = new File(fileName);
-
         try {
-            Reader reader = new FileReader(dataF);
+            Reader reader = new FileReader (fl1);
             Closeable resource = reader;
-            BufferedReader br = new BufferedReader(reader);
+            BufferedReader br = new BufferedReader (reader);
             resource = br;
             line = null;
             int i = 0;
-            while ((line = br.readLine()) != null) {
-                String[] data = line.split(cvsSplitBy);
-                double[] oneRow = new double[data.length];
+            while ((line = br.readLine ()) != null) {
+                String[] data = line.split (cvsSplitBy);
+                oneRow.clear ();
+                double tstData = 0.0;
                 int j = 0;
-                if (data.length > 4 && data.length < 6) {
-                    oneRow[0] = Double.parseDouble(data[0].toString());
-                    oneRow[1] = Double.parseDouble(data[2].toString());
-                    oneRow[2] = Double.parseDouble(data[3].toString());
-                    oneRow[3] = Double.parseDouble(data[4].toString());
-                    eleData1.add(oneRow);
-                    elename.add(data[1].toString());
-                    elementName.add(data[1].toString());
+                if ( !data[0].contains ("#") ) {
+                    tstData = Double.parseDouble (data[1].toString ());
+                    oneRow.add (tstData);
+                    tstData = Double.parseDouble (data[2].toString ());
+                    oneRow.add (tstData);
+                    tstData = Double.parseDouble (data[3].toString ());
+                    oneRow.add (tstData);
+                    tstData = Double.parseDouble (data[4].toString ());
+                    oneRow.add (tstData);                    
+                    eleData1.addAll (oneRow);
+                    elename.add (data[0].toString ());
+                    elementName.add (data[0].toString ());
+                    datType.put (data[0].toString (), 0);
                 }
             }
+            resource.close ();
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(MatGeneratorController.class.getName()).log(
+            Logger.getLogger (MatGeneratorController.class.getName ()).log (
                     Level.SEVERE, null, ex);
         } catch (IOException ex) {
-            Logger.getLogger(MatGeneratorController.class.getName()).log(
+            Logger.getLogger (MatGeneratorController.class.getName ()).log (
                     Level.SEVERE, null, ex);
         }
-        materials.getItems().clear();
-        materials.setMaxWidth(60);
-        materials.setMaxHeight(130);
-        materials.setLayoutX(30);
-        materials.setLayoutY(90);
-        materials.setItems(elementName);
-        selMat.setLayoutX(30);
-        selMat.setLayoutY(60);
-        selMe.setLayoutX(105);
-        selMe.setLayoutY(155);
-        matSelected.getItems().clear();
-        matSelected.setMaxWidth(60);
-        matSelected.setMaxHeight(130);
-        matSelected.setLayoutX(300);
-        matSelected.setLayoutY(90);
-        clsMe.setLayoutX(380);
-        clsMe.setLayoutY(110);
-        delMe.setLayoutX(380);
-        delMe.setLayoutY(140);
-        acceptMe.setLayoutX(380);
-        acceptMe.setLayoutY(170);
-        matGenPane.getChildren().addAll(selMat, materials, selMe, matSelected,
+
+        //---------------
+        try {
+            Reader reader = new FileReader (fl2);
+            Closeable resource = reader;
+            BufferedReader br = new BufferedReader (reader);
+            resource = br;
+            line = null;
+            int i = 0;
+            while ((line = br.readLine ()) != null) {
+                String[] data = line.split (cvsSplitBy);
+                int j = 0;
+                double tstData = 0.0;
+                oneRow.clear ();
+                if ( !data[0].contains ("#") ) {
+                    tstData = Double.parseDouble (data[1].toString ());
+                    oneRow.add (tstData);
+                    tstData = Double.parseDouble (data[2].toString ());
+                    oneRow.add (tstData);
+                    tstData = Double.parseDouble (data[3].toString ());
+                    oneRow.add (tstData);
+                    int dLim = (int) (2 * tstData);
+                    for ( int jj = 0; jj < dLim; jj++ ) {
+                        tstData = Double.parseDouble (data[4 + jj].toString ());
+                        oneRow.add (tstData);
+                    }
+                    eleData2.addAll (oneRow);
+                    elename.add (data[0].toString ());
+                    elementName.add (data[0].toString ());
+                    datType.put (data[0].toString (), 1);
+                    System.out.println(elementName.get (elementName.size ()-1));
+                }
+            }
+            resource.close ();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger (MatGeneratorController.class.getName ()).log (
+                    Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger (MatGeneratorController.class.getName ()).log (
+                    Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void selMat() {
+        String fileName1 = "Materials/VistaElements.mat";
+        String fileName2 = "Materials/VistaCompound.mat";
+        int dLen = 0;
+        File dataF1 = null;
+        File dataF2 = null;
+        String line = null;
+        String cvsSplitBy = ",";
+        File recordsDir = new File ("Materials");
+        if ( !recordsDir.exists () ) {
+            recordsDir.mkdirs ();
+        }
+
+        dataF1 = new File (fileName1);
+        dataF2 = new File (fileName2);
+        getdataContents (dataF1, dataF2);
+        
+        materials.getItems ().clear ();
+        materials.setMaxWidth (180);
+        materials.setMaxHeight (130);
+        materials.setLayoutX (30);
+        materials.setLayoutY (90);
+        materials.setItems (elementName);
+        selMat.setLayoutX (30);
+        selMat.setLayoutY (60);
+        selMe.setLayoutX (225);
+        selMe.setLayoutY (155);
+        matSelected.getItems ().clear ();
+        matSelected.setMaxWidth (60);
+        matSelected.setMaxHeight (130);
+        matSelected.setLayoutX (300);
+        matSelected.setLayoutY (90);
+        clsMe.setLayoutX (380);
+        clsMe.setLayoutY (110);
+        delMe.setLayoutX (380);
+        delMe.setLayoutY (140);
+        acceptMe.setLayoutX (380);
+        acceptMe.setLayoutY (170);
+        matGenPane.getChildren ().addAll (selMat, materials, selMe, matSelected,
                 clsMe, delMe, acceptMe);
 
-        selMe.addEventHandler(MouseEvent.MOUSE_CLICKED, evt -> {
-            materials.getSelectionModel().selectedItemProperty().
-                    addListener(new ChangeListener<String>() {
+        selMe.addEventHandler (MouseEvent.MOUSE_CLICKED, evt -> {
+            materials.getSelectionModel ().selectedItemProperty ().
+                    addListener (new ChangeListener<String> () {
                         @Override
                         public void changed(
                                 ObservableValue<? extends String> ov,
                                 String old_val, String new_val) {
-                            eleSelectName.add(new_val);
-                            int IDX = materials.getSelectionModel().getSelectedIndex();
-                            double[] oneRow = new double[4];
-                            for (int ii = 0; ii < 4; ii++) {
-                                oneRow[ii] = eleData1.get(IDX)[ii];
-                            }
-                            eleData2.add(oneRow);
-                            matSelected.setItems(eleSelectName);
+                            eleSelectName.add (new_val);
+                            int IDX = materials.getSelectionModel ().
+                                    getSelectedIndex ();
+                            // double[] oneRow = new double[4];
+                            oneRow.clear ();                            
+                            matSelected.setItems (eleSelectName);
                         }
                     });
-            eleSelectName.clear();
+            eleSelectName.clear ();
         });
 
-        clsMe.addEventHandler(MouseEvent.MOUSE_CLICKED, evt -> {
-            matSelected.getItems().clear();
+        clsMe.addEventHandler (MouseEvent.MOUSE_CLICKED, evt -> {
+            matSelected.getItems ().clear ();
         });
 
-        delMe.addEventHandler(MouseEvent.MOUSE_CLICKED, evt -> {
+        delMe.addEventHandler (MouseEvent.MOUSE_CLICKED, evt -> {
             int selIDX = -1;
-            selIDX = matSelected.getSelectionModel().getSelectedIndex();
-            System.out.println(selIDX);
-            matSelected.getItems().remove(selIDX);
+            selIDX = matSelected.getSelectionModel ().getSelectedIndex ();
+            System.out.println (selIDX);
+            matSelected.getItems ().remove (selIDX);
         });
 
-        acceptMe.addEventHandler(MouseEvent.MOUSE_CLICKED, evt -> {
-            eleSelectName.addAll(matSelected.getSelectionModel().
-                    getSelectedItems());
-            myGUI.setArray(eleSelectName, eleData2);
-            matGenPane.getChildren().clear();
+        acceptMe.addEventHandler (MouseEvent.MOUSE_CLICKED, evt -> {
+            eleSelectName.addAll (matSelected.getSelectionModel ().
+                    getSelectedItems ());
+            //myGUI.setArray (eleSelectName, eleData2);
+            matGenPane.getChildren ().clear ();
         });
     }
 }
